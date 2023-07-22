b5a8a9c434d8b823245aff82ad69421f0ad65772
Hemant Tyagi <91683575+tyagigop@users.noreply.github.com>
2023-07-22 19:21:26 +0530

    Update settings.py

--- counter/settings.py
+++ counter/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            BASE_DIR / 'TEMPLATES',
+            BASE_DIR / 'templates',
         ],
         "APP_DIRS": True,
         "OPTIONS": {
